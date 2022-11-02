d4ce50a6a890451c312edc2391faebbaaef82ace
sanyue <92298701+sanyuezoe@users.noreply.github.com>
2022-11-02 11:59:03 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,115 @@
+export const catsData = [
+    {
+        emotionTags: ["cute"],
+        image: "cute.jpg",
+        alt: "A cute archie",
+    },
+    {
+        emotionTags: ["cute", "silly"],
+        image: "cute1.jpg",
+        alt: "A cute tutu also silly",
+    },
+    {
+        emotionTags: ["cute", "sajiao"],
+        image: "cute2.jpg",
+        alt: "A cute archie again",
+    },
+    {
+        emotionTags: ["cute"],
+        image: "cute3.jpg",
+        alt: "A tutu in a caomei house",
+    },
+    {
+        emotionTags: ["cute", "silly"],
+        image: "cute4.jpg",
+        alt: "An archie bei zhualian",
+    },
+    {
+        emotionTags: ["lazy", "relaxed"],
+        image: "lazy.jpg",
+        alt: "An archie lying",
+    },
+    {
+        emotionTags: ["lazy", "pretty"],
+        image: "lazy1.jpg",
+        alt: "A tutu lying",
+    },
+    {
+        emotionTags: ["lazy", "sleepy"],
+        image: "lazy2.jpg",
+        alt: "A tutu lying sleeply",
+    },
+    {
+        emotionTags: ["lazy"],
+        image: "lazy3.jpg",
+        alt: "A tutu lying again",
+    },
+    {
+        emotionTags: ["pretty"],
+        image: "pretty.jpg",
+        alt: "An archie pretty",
+    },
+    {
+        emotionTags: ["pretty", "cute"],
+        image: "pretty1.jpg",
+        alt: "A tutu pretty",
+    },
+    {
+        emotionTags: ["sajiao", "relaxed"],
+        image: "sajiao.jpg",
+        alt: "A cat sajiao",
+    },
+    {
+        emotionTags: ["sajiao"],
+        image: "sajiao1.jpg",
+        alt: "A cat sajiao again",
+    },
+    {
+        emotionTags: ["sajiao", "lazy"],
+        image: "sajiao2.jpg",
+        alt: "A cat sajiao three",
+    },
+    {
+        emotionTags: ["sajiao", "relaxed"],
+        image: "sajiao3.jpg",
+        alt: "A cat sajiao four",
+    },
+    {
+        emotionTags: ["silly"],
+        image: "silly.jpg",
+        alt: "A silly tutu",
+    },
+    {
+        emotionTags: ["silly"],
+        image: "silly1.jpg",
+        alt: "A silly tutu again",
+    },
+    {
+        emotionTags: ["silly", "relaxed"],
+        image: "silly2.jpg",
+        alt: "A silly mengde",
+    },
+    {
+        emotionTags: ["silly"],
+        image: "silly3.jpg",
+        alt: "A silly mengde",
+    },
+    {
+        emotionTags: ["sleepy", "cute"],
+        image: "sleepy.jpg",
+        alt: "A cat looking sleepy",
+    },
+    {
+        emotionTags: ["sleepy", "lazy"],
+        isRandom: true,
+        image: "sleepy1.jpg",
+        alt: "A cat looking sleepy",
+    },
+    {
+        emotionTags: ["sleepy"],
+        isRandom: true,
+        image: "sleepy2.jpg",
+        alt: "A cat looking sleepy",
+    },
+
+]
